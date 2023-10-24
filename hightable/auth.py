5a2b65482974f50a90824452272715d2042e0e7c
Bronyte <117386174+Bronyte@users.noreply.github.com>
2023-10-24 11:07:41 +0000

    added methods to the auth /sign-up route. Added additonal fields, and initiated registration logic.

--- hightable/auth.py
+++ hightable/auth.py
@@ -8,12 +8,22 @@ def login():
 
 @auth.route('/logout')
 def logout():
-    return "<p>logout </p>"
+    return "<p>logout</p>"
 
-@auth.route('/sign-up')
+@auth.route('/sign-up', methods = [ 'GET', 'POST'])
 def sign_up():
     if request.method == 'POST':
         email = request.form.get('email')
-        firstName = request.form.get('password1')
+      
+        firstName = request.form.get('firstName')
+        lastName = request.form.get('lastName')
+      
+        password1 = request.form.get('password1')
+        password2 = request.form.get('password2')
 
+        if password1 == password2:
+          flash("Passwords match")
+          # Add registration logic here
+        else:
+          flash("Passwords do not match!")
     return render_template("sign_up.html")
